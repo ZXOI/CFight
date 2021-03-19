3054f3d53094291b4c50504ae8953c869ee0bc99
Zhou Xuecheng <Zxc200611@yandex.com>
2021-03-19 14:03:01 +0800

    Add files via upload

---
+++ CFight-1.0/CFight-calc.js
@@ -0,0 +1,214 @@
+function get_chess_by_pos(_x, _y){
+	for(let i in chesses){
+		let e = chesses[i];
+		if(e.x == _x && e.y == _y){
+			return i;
+		}
+	}
+	return -1;
+}
+
+function Chess(_x, _y, _t){
+	this.x = _x;
+	this.y = _y;
+	this.team = _t;
+	this.at_border = new Array(this.x==boardh,this.y==1,this.x==1,this.y==boardw);
+	this.alive = 1;
+	this.tprc = 255;
+	
+	this.update_atborder = () => {
+		for(let d=0; d<4; d+=1) {
+			let px = this.x+dir[d][0], py = this.y+dir[d][1];
+			if(px<=0 || py<=0 || px>boardh || py>boardw){
+				this.at_border[d] = true;
+			}
+			else {
+				this.at_border[d] = false;
+			}
+		}
+	}
+	this.can_move = (d) => {
+		if(d == -1) {
+			return 0;
+		}
+		if(this.alive == 0){
+			return 0;
+		}
+		console.log(this.at_border);
+		if(this.at_border[d]){
+			return 1;
+		}
+		if(map[this.x][this.y] & (1<<d)){
+			return 1;
+		}
+		let chess_cnt=0;
+		let i = this.x, j = this.y;
+		let dis = 0;
+		for(;; i += dir[d][0],j += dir[d][1], dis++){
+			let k = get_chess_by_pos(i + dir[d][0], j + dir[d][1]);
+			if(k!=-1){
+				chess_cnt ++;
+			}
+			let _i = i + dir[d][0] * chess_cnt;
+			let _j = j + dir[d][1] * chess_cnt;
+			//console.log(_i,_j);
+			if(_i < 1 || _i > boardh || _j < 1 || _j > boardw || (map[_i][_j] & (1<<d))){
+				return dis;
+			}
+		}
+	}
+	this.check_alive = () => {
+		if(this.x < 1 || this.y < 1 || this.x > boardh || this.y > boardw){
+			this.alive = 0;
+		}
+	}
+	this.draw_possibilities = () => {
+		for(let d=0; d < 4; d+=1) {
+			let maxlen = this.can_move(d);
+			let px = this.x;
+			let py = this.y;
+			for (let l=0; l <maxlen; l+=1) {
+				px += dir[d][0];
+				py += dir[d][1];
+				let tx=px*blockw;
+				let ty=py*blockw;
+				ctx.fillStyle = colors.possiblilty + "33";
+				ctx.fillRect(ty, tx, blockw, blockw);
+			}
+		}
+	}
+	this.move = (d, dis) => {
+		// let chess_on_way = new Array();
+		// let i = this.x, j = this.y;
+		// let mvcnt = 0;
+		// for(;mvcnt < dis; i += dir[d][0],j += dir[d][1],mvcnt += 1) {
+		// 	let k = get_chess_by_pos(i + dir[d][0], j + dir[d][1]);
+		// 	if(k!=-1)console.log(chesses[k].x,chesses[k].y);
+		// 	if(k!=-1) {
+		// 		chess_on_way.push(k);
+		// 	}
+		// }
+		// console.log(chess_on_way);
+		// for(let th in chess_on_way) {
+		// 	console.log(th,chess_on_way[th]);
+		// 	chesses[chess_on_way[th]].move(d,dis-Math.abs(chesses[chess_on_way[th]].x-this.x)-Math.abs(chesses[chess_on_way[th]].y-this.y)+th+1);
+		// }
+		// this.x += dis * dir[d][0];
+		// this.y += dis * dir[d][1];
+		for(let mvcnt = 0; mvcnt < dis; mvcnt += 1) {
+			let last = get_chess_by_pos(this.x, this.y);
+			let i = this.x + dir[d][0], j = this.y + dir[d][1];
+			let cnt = 0;
+			while(get_chess_by_pos(i,j)!=-1){
+				let tmp = get_chess_by_pos(i,j);
+				chesses[last].x += dir[d][0];
+				chesses[last].y += dir[d][1];
+				last = tmp;
+				i += dir[d][0];
+				j += dir[d][1];
+				cnt += 1;
+			}
+			chesses[last].x += dir[d][0];
+			chesses[last].y += dir[d][1];
+			//console.log(cnt);
+		}
+	}
+	this.move_to = (x, y) => {
+		let dx=x-this.x, dy=y-this.y;
+		let d = getDir(this.x, this.y, x, y);
+		if(d == -1) {
+			return 0;
+		}
+		//console.log("Chess move dir ",d," ,dis ", Math.abs(dx+dy)," to ",x,y);
+		if(this.can_move(d) >= Math.abs(dx+dy)) {
+			this.move(d, Math.abs(dx+dy));
+			return 1;
+		}
+		// this.check_alive()
+		return 0;
+	}
+	this.can_move_to = (x, y) => {
+		let d = getDir(this.x, this.y, x, y)
+		return d!=-1 && this.can_move(d)>=abs(this.x-x)+abs(this.y-y);
+	}
+	this.draw = () => {
+		let x=this.x*blockw+blockw/2;
+		let y=this.y*blockw+blockw/2;
+		drawcirc(y, x, chessr1, chessr2, colors.teams[this.team][0] + toHex(Math.max(this.tprc,1)), colors.teams[this.team][1] + toHex(Math.max(this.tprc - toDec("CC"),1)));
+	}
+	this.update = () => {
+		this.update_atborder();
+		this.check_alive();
+		if(!this.alive) {
+			this.tprc -= 2;
+			this.tprc = Math.max(this.tprc,0);
+		}
+		if(!this.tprc) {
+			let k = get_chess_by_pos(this.x,this.y);
+			//console.log("Deling ",k);
+			chesses[k] = chesses[chesses.length - 1];
+			chesses.pop();
+		}
+	}
+	this.check_win = () => {
+		if(chessmap[this.x][this.y] != this.team+10 && chessmap[this.x][this.y] > 10)
+		{
+			return 1;
+		}
+		return 0;
+	}
+}
+
+function add_chess() {
+	for(let i = 0; i <= boardh; i++) {
+		for(let j = 0; j <= boardw; j++) {
+			// console.log(i,j,chessmap[i][j]);
+			if(chessmap[i][j] != 0 && chessmap[i][j] <= 10) {
+				chesses.push(new Chess(i, j, chessmap[i][j]));
+			}
+		}
+	}
+}
+function update_chess() {
+	for(let i in chesses) {
+		chesses[i].update();
+	}
+	for(let i in chesses) {
+		if(chesses[i].check_win()) {
+			win = chesses[i].team;
+		}
+	}
+}
+
+function player_move(){
+	if(chesses[selectedchess].move_to(selectedx,selectedy))
+	{
+		nowmove = (nowmove % team_cnt) + 1;
+		return 1;
+	}
+	return 0;
+}
+
+function mouse_select(x, y) {
+	if(selectedx == x && selectedy == y) {
+		selectedx = -1;
+		selectedy = -1;
+		selectedchess = -1;
+		return;
+	}
+	selectedx = x;
+	selectedy = y;
+	let selectchess = get_chess_by_pos(x, y);
+	// console.log(selectchess);
+	// if(selectedchess != -1 && chesses[selectedchess].team == nowmove && chesses[selectedchess].can_move(getDir()))
+	if(selectedchess != -1 && chesses[selectedchess].team == nowmove) {
+		//console.log("Move!");
+		let k = player_move();
+		if(k) {
+			selectedchess = -1;
+			return;
+		}
+	}
+	//console.log("Select!");
+	selectedchess=selectchess;
+}
